4c702e55b76545ee0ddcd89e173e039f752401f0
UltimatOne <116150495+UltimatOne@users.noreply.github.com>
2022-12-18 22:21:39 +0100

    feat: plant item

--- src/datas/plantList.js
+++ src/datas/plantList.js
@@ -1,56 +1,86 @@
+import monstera from '../assets/monstera.jpg'
 export const plantList = [
     {
-		name: 'monstera',
+		name: 'Monstera',
 		category: 'classique',
 		id: '1ed',
-		isBestSale: true
+		isBestSale: true,
+		light: 2,
+		water: 3,
+		cover: monstera
 	},
 	{
-		name: 'ficus lyrata',
+		name: 'Ficus Lyrata',
 		category: 'classique',
 		id: '2ab',
-		isBestSale: false
+		isBestSale: false,
+		light: 3,
+		water: 1,
+		cover: monstera
 	},
 	{
-		name: 'pothos argenté',
+		name: 'Pothos Argenté',
 		category: 'classique',
 		id: '3sd',
-		isBestSale: false
+		isBestSale: false,
+		isSpecialOffer: true,
+		light: 1,
+		water: 2,
+		cover: monstera
 	},
 	{
-		name: 'yucca',
+		name: 'Yucca',
 		category: 'classique',
 		id: '4kk',
-		isBestSale: false
+		isBestSale: false,
+		light: 3,
+		water: 1,
+		cover: monstera
 	},
 	{
-		name: 'olivier',
+		name: 'Olivier',
 		category: 'extérieur',
 		id: '5pl',
-		isBestSale: false
+		isBestSale: false,
+		light: 3,
+		water: 1,
+		cover: monstera
 	},
 	{
-		name: 'géranium',
+		name: 'Géranium',
 		category: 'extérieur',
 		id: '6uo',
-		isBestSale: true
+		isBestSale: true,
+		light: 2,
+		water: 2,
+		cover: monstera
 	},
 	{
-		name: 'basilique',
+		name: 'Basilique',
 		category: 'extérieur',
 		id: '7ie',
-		isBestSale: false
+		isBestSale: false,
+		isSpecialOffer: true,
+		light: 2,
+		water: 3,
+		cover: monstera
 	},
 	{
-		name: 'aloe',
+		name: 'Aloe',
 		category: 'plante grasse',
 		id: '8fp',
-		isBestSale: false
+		isBestSale: false,
+		light: 2,
+		water: 1,
+		cover: monstera
 	},
 	{
-		name: 'succulente',
+		name: 'Succulente',
 		category: 'plante grasse',
 		id: '9vn',
-		isBestSale: false
+		isBestSale: false,
+		light: 2,
+		water: 1,
+		cover: monstera
 	}
   ];
